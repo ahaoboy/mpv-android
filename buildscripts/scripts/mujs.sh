97d92a33389222b4af82b42ee209db19307217aa
ahaoboy <504595380@qq.com>
2024-05-16 16:29:15 +0800

    add mujs

--- buildscripts/scripts/mujs.sh
+++ buildscripts/scripts/mujs.sh
@@ -13,15 +13,3 @@ fi
 
 make HAVE_READLINE=no -j$cores
 make HAVE_READLINE=no DESTDIR="$prefix_dir" install
-
-# make pc only generates a partial pkg-config file because ????
-mkdir -p $prefix_dir/lib/pkgconfig
-make pc >$prefix_dir/lib/pkgconfig/mujs.pc
-cat >>$prefix_dir/lib/pkgconfig/mujs.pc <<'EOF'
-Name: mujs
-Description: MuJS embeddable Javascript interpreter
-Version: ${version}
-Libs: -L${libdir} -lmujs
-Cflags: -I${includedir}
-Libs.private: -lm
-EOF
